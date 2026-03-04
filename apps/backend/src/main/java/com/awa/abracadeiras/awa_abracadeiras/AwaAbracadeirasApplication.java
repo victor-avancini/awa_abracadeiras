fa32e95c3431925f817031d72e1be284bf1af3c7
Victor Hugo Avancini <140919975+victor-avancini@users.noreply.github.com>
2026-03-04 16:12:23 -0300

    feat: bootstrap spring boot backend

---
+++ apps/backend/src/main/java/com/awa/abracadeiras/awa_abracadeiras/AwaAbracadeirasApplication.java
@@ -0,0 +1,13 @@
+package com.awa.abracadeiras.awa_abracadeiras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwaAbracadeirasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwaAbracadeirasApplication.class, args);
+	}
+
+}
